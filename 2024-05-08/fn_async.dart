8e62eb09bb638cc84fdd4d610ed49492265e52cd
cheng.li <licheng@hbccy.com.cn>
2024-05-08 18:12:25 +0800

    添加 pubspec.yaml 添加http请求库，请求github的公共接口

---
+++ 2024-05-08/fn_async.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<void> fetchGithubData() async {
+  try {
+    final String url = "https://api.github.com/users/flutter/repos";
+    // 发送get请求
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      // 解析json数据
+      final List<dynamic> data = jsonDecode(response.body);
+      print(data);
+      // 打印数据
+    } else {
+      print("Failed to load data");
+    }
+
+  } on http.ClientException catch (e) {
+     print('Caught an exception: $e');  
+  }
+}
+
+Future<String> fetchData() async {
+  await Future.delayed(Duration(seconds: 2));
+  return "Data fetched successfully";
+}
+
+
+
+
+void main() async {
+  final String data = await fetchData();
+  print(data);
+
+  await fetchGithubData();
+
+}
