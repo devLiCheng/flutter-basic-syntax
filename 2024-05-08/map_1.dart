50bf41bb1eec3bf70588ceec72b142b6624b68a2
cheng.li <licheng@hbccy.com.cn>
2024-05-08 15:55:35 +0800

    add list iterable

---
+++ 2024-05-08/map_1.dart
@@ -0,0 +1,19 @@
+void main() {
+  Map m = {};
+  print(m);
+
+  // List<int> number = [1,2,3];
+  // List<double> = numbers.map((num) => num.toDouble()).toList();
+  // print(numbers);
+
+  // 2.
+  List<int> numbers = [1,2,3];
+  print(numbers is List<int>); 
+  Iterable<double> numbers2 = numbers.map((num) => num.toDouble()).toList();
+  print(numbers2 is Iterable<double>);
+  print(numbers2); // [1.0, 2.0, 3.0]
+
+  List<double> list_2 = numbers.map((num) => num.toDouble()).toList();
+  print(list_2 is List<double>);
+  print(list_2);
+}
